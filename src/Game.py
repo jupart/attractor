280f6d3c5bae542894b0eefad88e08c23d18c007
Justin Partain <justinwpartain1@gmail.com>
2017-05-18 15:55:10 -0400

    Pre git-flow init

---
+++ src/Game.py
@@ -0,0 +1,19 @@
+from kivy.uix.widget import Widget
+
+import kivent_core
+import kivent_cymunk
+from kivent_core.systems.position_systems import PositionSystem2D
+from kivent_core.systems.rotate_systems import RotateSystem2D
+from kivent_core.systems.renderers import RotateRenderer
+from kivent_core.systems.gamesystem import GameSystem
+from kivent_core.managers.resource_managers import texture_manager
+from kivent_core.gameworld import GameWorld
+
+
+class AttractorGame(Widget):
+    def __init__(self, **kwargs):
+        super(AttractorGame, self).__init__(**kwargs)
+        self.world.init_gameworld([], callback=self.initGame)
+
+    def initGame(self):
+        pass
