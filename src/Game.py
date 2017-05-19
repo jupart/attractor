8a763023cbaa8103277fd46bede6a5b2f5b01a04
Justin Partain <justinwpartain1@gmail.com>
2017-05-19 16:23:03 -0400

    Working some boilerplate in

--- src/Game.py
+++ src/Game.py
@@ -13,7 +13,33 @@ from kivent_core.gameworld import GameWorld
 class AttractorGame(Widget):
     def __init__(self, **kwargs):
         super(AttractorGame, self).__init__(**kwargs)
-        self.gameworld.init_gameworld([], callback=self.initGame)
+        self.gameworld.init_gameworld([], callback=self.init_game)
 
-    def initGame(self):
+    def init_game(self):
+        self.setup_states()
+        self.gameworld.state = 'menu'
+        self.load_models()
+        self.create_entities()
+
+    def setup_states(self):
+        self.gameworld.add_state(state_name='menu',
+                                 systems_added=['rotate_renderer'],
+                                 systems_removed=[],
+                                 systems_paused=['position', 'camera'],
+                                 systems_unpaused=['rotate_renderer'],
+                                 screenmanager_screen='menu_screen')
+        self.gameworld.add_state(state_name='play',
+                                 systems_added=[],
+                                 systems_removed=[],
+                                 systems_paused=[],
+                                 systems_unpaused=[],
+                                 screenmanager_screen='play_screen')
+
+    def load_models(self):
         pass
+
+    def create_entities(self):
+        pass
+
+    def go_to_play_screen(self):
+        self.gameworld.state = 'play'
---
+++ src/Screens.py
@@ -0,0 +1,11 @@
+from kivy.uix.screenmanager import Screen
+
+
+class MenuScreen(Screen):
+    def __init__(self, **kwargs):
+        super(MenuScreen, self).__init__(**kwargs)
+
+
+class PlayScreen(Screen):
+    def __init__(self, **kwargs):
+        super(PlayScreen, self).__init__(**kwargs)
